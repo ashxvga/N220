7436b6dbb84155b268496e5f0d4ca0845953d9db
Ashley Rodriguez Vega <ashleyrvega16@gmail.com>
2024-09-10 15:58:25 -0400

    Added functions with and without parameters

--- week-3/app.js
+++ week-3/app.js
@@ -11,3 +11,31 @@ function simpleFunctionTwo()
     document.querySelector("body").innerHTML += "This function does not take parameters.";
 }
 simpleFunctionTwo();
+
+function sayHelloWorld()
+{
+    console.log("Hello, world");
+}
+//started using parameters
+
+sayHelloWorld();
+
+function sayFavoriteBand(bandName)
+{
+    document.getElementById("favBand").innerHTML += bandName + "<br/>"
+}
+
+sayFavoriteBand("Blackpink");
+sayFavoriteBand("Cigarettes after sx");
+sayFavoriteBand("Tipling rock");
+
+function sayMyName(myName)
+{
+    console.log(myName);
+}
+
+sayMyName("Ashley");
+sayMyName("Say my name");
+sayMyName("Say my name");
+sayMyName("Say my name");
+sayMyName("If no one is around you");
