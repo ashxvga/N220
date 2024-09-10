17a9dbeb9fe4faba3327a931b2cd3d3196983224
Ashley Rodriguez Vega <ashleyrvega16@gmail.com>
2024-09-09 22:46:11 -0400

    testing 2

--- week-2/homework-one/homework1Script.js
+++ week-2/homework-one/homework1Script.js
@@ -2,4 +2,6 @@ let ticketCost = 14.00;
 let numberOfTickets = 3;
 let total = ticketCost * numberOfTickets;
 
+console.log(numberOfTickets)
+
 document.getElementById("ticketNum").innerHTML += "Tickets: " + numberOfTickets;
