f91d6fb1de0f4f0e801414306ccd8b5d137f1371
Ashley Rodriguez Vega <ashleyrvega16@gmail.com>
2024-09-10 15:35:04 -0400

    called simpleFunctionTwo();

--- week-3/app.js
+++ week-3/app.js
@@ -3,3 +3,11 @@ function simpleFunctionOne()
     //code here
     document.getElementById("changeTxt").innerHTML = "Don't change this paragraph."
 }
+
+simpleFunctionOne();
+
+function simpleFunctionTwo()
+{
+    document.querySelector("body").innerHTML += "This function does not take parameters.";
+}
+simpleFunctionTwo();
