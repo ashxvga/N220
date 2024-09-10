e0efcb34aab2dfdaf708a28ae3221241e4af2a43
Ashley Rodriguez Vega <ashleyrvega16@gmail.com>
2024-09-10 16:57:39 -0400

    added more functions

--- week-3/app.js
+++ week-3/app.js
@@ -39,3 +39,71 @@ sayMyName("Say my name");
 sayMyName("Say my name");
 sayMyName("Say my name");
 sayMyName("If no one is around you");
+
+//functions that do calculations
+
+function calcRecPerimeter(height, width)
+{
+    //const perimeter = 2 * height + 2 * width;
+    const perimeter = 2 * (height + width);
+    document.querySelector("#panswer").innerHTML += perimeter;
+}
+
+calcRecPerimeter(2,4);
+
+function calcArea()
+{
+    const height = document.getElementById("height").value;
+    const width = document.getElementById("width").value;
+    console.log("height", height);
+    console.log("width", width);
+    document.querySelector("#answer").innerHTML += "Area: " + height * width + "<br/>"
+    //`Area: ${height * width};`
+}
+
+function calRecAreaAndPeri(height, width)
+{
+    const perimeter = 2 * (height + width);
+    const area = height * width;
+
+    document.getElementById("multiAnswer").innerHTML += `
+    Height: ${height} <br/>
+    Width: ${width} <br/>
+    Perimeter: ${perimeter} <br/>
+    Area: ${area} <br/><br/>
+    `;
+}
+calRecAreaAndPeri(5,4);
+calRecAreaAndPeri(2,8);
+
+function myFunction()
+{
+    alert("Who goes there?");
+}
+
+function wizard(name, occupation)
+{
+    //confirm("Welcome" + name + ", the " + occupation);
+    confirm(`Welcome ${name}, the ${occupation}`);
+    document.getElementByIdO("future").innerHTML += `Welcome ${name}, the ${occupation}!`;
+}
+
+function addItem()
+{
+    const newItem = prompt("Add Item:");
+    console.log(newItem);
+}
+
+function returnValue(color)
+{
+    return color;
+}
+alert(returnValue("yellow"))
+
+function calcCircleArea(radius)
+{
+    return 2 * Math.PI * radius;
+}
+
+document.getElementById("useOne").innerHTML += calcCircleArea(2);
+document.getElementById("useTwo").innerHTML += calcCircleArea(4);
