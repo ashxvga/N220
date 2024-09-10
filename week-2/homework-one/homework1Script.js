1c909c446bd85578af54813247bbe9aab0532e56
Ashley Rodriguez Vega <ashleyrvega16@gmail.com>
2024-09-10 13:52:35 -0400

    Completed the first and second problems

--- week-2/homework-one/homework1Script.js
+++ week-2/homework-one/homework1Script.js
@@ -2,6 +2,22 @@ let ticketCost = 14.00;
 let numberOfTickets = 3;
 let total = ticketCost * numberOfTickets;
 
-console.log(numberOfTickets)
-
 document.getElementById("ticketNum").innerHTML += "Tickets: " + numberOfTickets;
+document.getElementById("ticketCost").innerHTML += "Cost: " + ticketCost;
+document.getElementById("ttCost").innerHTML += "Total: " + total;
+
+//for the second problem
+let bankAccount = 235.83;
+let jacket = 70;
+let sweater = 50
+let shirt = 35;
+let pants = 75;
+let skirt = 49;
+let sandals = 30;
+let shoes = 60;
+
+document.getElementById("bank").innerHTML += "After purchasing the outfit, my bank account is $" + bankAccount - (shirt+pants+shoes);
+let remainingFunds = bankAccount - (shirt+pants+shoes);
+let canBuyJacket = remainingFunds - jacket >= 0;
+document.getElementById("addJacket").innerHTML += "Add Jacket: " + canBuyJacket;
+
