614c9a2ff38c9ce78abecdc4d6d71df214f914e9
Ashley Rodriguez Vega <ashleyrvega16@gmail.com>
2024-11-12 16:01:01 -0500

    toggle section visibility

---
+++ week-12/thebox.js
@@ -0,0 +1,54 @@
+//console.log('Dont look in the box');
+const theBoxRef = document.getElementById("theBox");
+
+theBoxRef.style.border = "3px solid black";
+theBoxRef.style.minHeight = "300px";
+theBoxRef.style.display = "grid";
+theBoxRef.style.gridTemplateColumns = "1fr 1fr 1fr";
+
+function addToBox(){
+    const newThing = prompt("Thing to Add: ");
+
+    const newDiv= document.createElement("div");
+    newDiv.style.height = "100px";
+    newDiv.style.width = "100px";
+    newDiv.style.backgroundColor = "lightgrey";
+    newDiv.style.margin = "5px";
+    //adds the class to the box
+    newDiv.classList.add("box");
+    newDiv.innerHTML = newThing;
+
+    theBoxRef.appendChild(newDiv);
+    const allBoxes = document.querySelectorAll(".box");
+    for(let i =0; i < allBoxes.length; i++)
+        {
+            const box = allBoxes[i];
+            box.onclick = clickBox;
+        }
+}
+
+function clickBox(e)
+{
+    if(e.currentTarget.style.backgroundColor === "red"){
+        e.currentTarget.style.backgroundColor = "lightgrey";
+    }
+    else
+    {
+        e.currentTarget.style.backgroundColor = "red";
+    }
+    console.log(e.currentTarget);
+    e.currentTarget.style.backgroundColor = "red";
+}
+
+function toggleTheBoxHidden()
+{
+    if(theBoxRef.style.display !== "none")
+    {
+        theBoxRef.style.display = "none";
+    }
+    else
+    {
+        theBoxRef.style.display = "grid";
+    }
+    //theBoxRef.outerHTML += `<button onclick="toggleTheBoxHidden()"> Toggle Hidden </button>`;
+}
