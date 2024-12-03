9318b7d839a7b3cd5937b631ea967b292bfb6cfb
Ashley Rodriguez Vega <ashleyrvega16@gmail.com>
2024-12-03 17:08:31 -0500

    added code to fix the logic of validating the password, fixed indentation of my loop, and added comments to keep track of what everything means

--- final-project/final.js
+++ final-project/final.js
@@ -3,58 +3,61 @@ function validatePassword(){
 
     //console.log(password.value);
 
-    //let validPassword = false;
+
+    let isNotEmpty = false;
     let isLongEnough = false;
     let containsNumber = false;
     let containsUpperCase = false;
 
+    //checks if the password has at least a character but not 8
+    //basically means not long enough, but at least it is not empty
+    if(passwordRef.value.length > 0 && passwordRef.value.length < 8)
+    {
+        isNotEmpty = true;
+    }
+    //not empty and long enough
     if(passwordRef.value.length >= 8)
     {
         isLongEnough = true;
+        isNotEmpty = true;
     }
-    for (let i = 0; i < passwordRef.value.length; i++)
-        {
-            const currentCharacter = passwordRef.value[i];
-            //is there at least one number
-            if(isNaN(currentCharacter)===false)
-            {
-                containsNumber = true;
-            }
-            //is there at least one lowercase letter
-            if (currentCharacter == currentCharacter.toUpperCase() && isNaN(currentCharacter))
-            {
-                containsUpperCase = true;
-            }
-            
-        }
-    // if(passwordRef.value.length===0)
-    // {
-    //     alert("Enter a valid password containing at least 8 characters, a capital letter, and a number.");
-    // }
-    // else if(containsUpperCase== false){
-    //     alert("Enter a valid password containing at least a capital letter");
-    // }
-    // else if(containsNumber == false)
-    // {
-    //     alert("Enter a valid password containing at least a number ");
-    // }
     
-    const validPassword = containsNumber && containsUpperCase && isLongEnough;
+    for (let i = 0; i < passwordRef.value.length; i++)
+    {
+        const currentCharacter = passwordRef.value[i];
+        //is there at least one number
+        if(isNaN(currentCharacter)===false)
+        {
+            containsNumber = true;
+        }
+        //is there at least one lowercase letter
+            
+        if (currentCharacter == currentCharacter.toUpperCase() && isNaN(currentCharacter))
+        {
+            containsUpperCase = true;
+        }
+            
+    } 
+    const validPassword = containsNumber && containsUpperCase && isNotEmpty && isLongEnough;
     if (validPassword)
     {
         alert("Woohoo!");
         passwordRef.value = "";
     }
-    else if(passwordRef.value.length===0){
+    //in case the password is empty
+    else if(isNotEmpty==false){
         alert("Enter a valid password containing at least 8 characters, a capital letter, and a number.");
     }
-    // else if(passwordRef.value.length> 0 && < 8){
-    //     alert("Enter a valid password containing at least 8 characters, a capital letter, and a number.");
-    // }
+    //in case is not long enough (8 characters)
+    else if(isLongEnough ==false){
+         alert("Enter a valid password that has at least 8 characters");
+    }
+    //in case it is missing a capital letter
     else if (containsUpperCase== false)
     {
         alert("Enter a valid password containing at least a capital letter");
     }
+    //in case it is missing a number
     else if(containsNumber == false){
         alert("Enter a valid password containing at least a number ");
     }
